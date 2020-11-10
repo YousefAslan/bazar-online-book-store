4b1f396cb4a2687c68f0549fd4e9235763593cec
YousefAslan <YousefAslan9@gmail.com>
2020-11-11 01:26:32 +0200

    Modify the status code to match the response content from the catalog server

--- catalog server/catalog_server.py
+++ catalog server/catalog_server.py
@@ -4,44 +4,61 @@ from server_configuration import *
 @catalog_server.route("/search/<string:topic>",methods=['GET'])
 def search(topic):
     books = Book.query.filter_by(topic= topic).all()
-    return books_search_schema.jsonify(books)
-
+    if len(books) > 0:
+        return books_search_schema.jsonify(books)
+    else:
+        return {"message": "There are no books under this topic"}, 410
+    
 @catalog_server.route("/lookup/<int:id>",methods=['GET'])
 def lookup(id):
-    books = Book.query.filter_by(id = id).first()
-    return book_lookup_schema.jsonify(books)
-
+    book = Book.query.get(id)
+    if book:
+        return book_lookup_schema.jsonify(book), 200
+    else:
+        return {"message" : "There is no book with this ID"}, 404
+    
 @catalog_server.route("/verify_item_in_stock/<int:id>",methods=['GET'])
 def verify_item_in_stock(id):
-    book = Book.query.filter_by(id = id).first()
-    return verify_schema.jsonify(book)
+    book = Book.query.get(id)
+    if book:
+        if book.quantity > 0:
+            book.quantity -= 1
+            db.session.commit()
+            return verify_schema.jsonify(book), 200
+        else:
+            return {"message": "This book is currently unavailable"}, 410
+    else:
+        return {"message": "There is no book with this ID"}, 404
 
 @catalog_server.route("/update/<int:id>",methods=['PUT'])
 def update(id):
     book = Book.query.get(id)
-    try:
+    if book:
         if book.quantity > 0:
             book.quantity -= 1
             db.session.commit()
             return {}, 204
         else:
-            return {"message": "Gone", "description": "This book is currently unavailable"}, 410
-    except:
-        return {"message": "Not Found", "description": "There is no book with this ID"}, 404
+            return {"message": "This book is currently unavailable"}, 410
+    else:
+        return {"message": "There is no book with this ID"}, 404
     
 
 @catalog_server.route("/append",methods=['POST'])    
 def append():
-    print(request)
-    title = request.json['title']
-    quantity = request.json['quantity']
-    cost = request.json['cost']
-    topic = request.json['topic']
+    try:
+        title = request.json['title']
+        quantity = request.json['quantity']
+        cost = request.json['cost']
+        topic = request.json['topic']
 
-    new_book = Book(title,quantity,cost,topic)
-    db.session.add(new_book)
-    db.session.commit()
-    return book_search_schema.jsonify(new_book)
+        new_book = Book(title,quantity,cost,topic)
+        db.session.add(new_book)
+        db.session.commit()
+        return book_search_schema.jsonify(new_book), 201
+    except expression as identifier:
+        return {"message" : "cant add this book"}, 405
+    
 
 @catalog_server.errorhandler(404)
 def resource_could_not_found(e):
