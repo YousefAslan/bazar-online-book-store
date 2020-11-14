68e1fe350b77aaa0474736a2f9b88108c2bd8b52
YousefAslan <YousefAslan9@gmail.com>
2020-11-14 13:19:40 +0200

    add comments into the front-end server

--- front-end server/front-end server.py
+++ front-end server/front-end server.py
@@ -17,26 +17,46 @@ catalog_server_port = 2310
 
 app = Flask(__name__)
 
-
 @app.route('/search/<string:topic>',methods = ['GET'])
 def search(topic):
+    """
+    mapping the coming get request on the route search/<topic> to the method search
+    The search method aims to send the request coming from the end-user to the catalog server to be processed there
+    and await a response from the catalog to be responded on the end-user.
+
+    """
     responce = requests.get(catalog_server_ip + ':' + str(catalog_server_port) + '/search/' + topic)
-    print(type(responce.json()))
-    print(responce.json())
     return jsonify(responce.json()), responce.status_code
 
 @app.route('/lookup/<int:id>',methods = ['GET'])
 def lookup(id):
+    """
+    mapping the coming get request on the route lookup/<id> to lookup method
+    It aims to send the incoming get request to the catalog server to deal with it
+    and response to the end-user
+
+    """
     responce = requests.get(catalog_server_ip + ':' + str(catalog_server_port) + '/lookup/' + str(id))
     return jsonify(responce.json()), responce.status_code
 
 @app.route('/buy/<int:id>',methods = ['PUT'])
 def buy(id):
+    """
+    mapping the coming put request on the route buy/<id> to buy method
+    Their purpose is to purchase this item based on the attached id and url by sending it to another server called order server
+    to handle the purchase process and with response which indicate if this process done or not
+
+    """
     responce = requests.put(order_server_ip + ':' + str(order_server_port) + '/buy/' + str(id))
     return jsonify(responce.json()), responce.status_code
 
 @app.route("/update/price/<int:id>",methods=['PUT'])
 def update_price(id):
+    """
+    mapping the coming put request on the route /update/price/<id> to update_price method
+    Their purpose is to send a put request to the catalog server to update the price of a spacefice item based on the id
+
+    """
     headers = {'Content-type': 'application/json'}
     jsons = request.json
     responce = requests.put(catalog_server_ip + ':' + str(catalog_server_port) + '/update/price/' + str(id), json= jsons, headers =headers)
@@ -44,6 +64,11 @@ def update_price(id):
 
 @app.route("/update/item/<int:id>",methods=['PUT'])
 def update_item_number(id):
+    """
+    mapping the coming put request on the route /update/item/<id> item_number method
+    Their purpose is to send a put request to the catalog server to update number of items avaibale from the product
+
+    """
     headers = {'Content-type': 'application/json'}
     jsons = request.json
     responce = requests.put(catalog_server_ip + ':' + str(catalog_server_port) + '/update/item/' + str(id), json= jsons, headers =headers)
@@ -51,11 +76,15 @@ def update_item_number(id):
 
 @app.errorhandler(404)
 def resource_could_not_found(e):
-    return jsonify({'error': 404}), 404
+    """
+    this method responce if the end user send a request asking from URL that deos not exist such as /update/name/id
+
+    """
+    return jsonify({'message': "server can't find the requested resource."}), 404
 
 @app.errorhandler(405)
 def method_not_allowed(e):
-    return jsonify({'error': 405}), 405
+    return jsonify({'message': " the server but is not supported by the target resource"}), 405
 
 if __name__ == '__main__':
   app.run(debug = True, port = 2309, host='0.0.0.0')
